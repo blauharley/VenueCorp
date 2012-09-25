09d14d7e54a0b750814de1785baea558ebf48f7d
Franz Josef <franz.josef.bruenner@gmail.com>
2012-09-25 14:02:41 +0200

    preparing map for event show

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,15 @@
+﻿
+window.onload = function(){
+  
+  if(document.getElementById('event_map')){
+    
+    var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+    var map = new google.maps.Map(document.getElementById("event_map"), mapOptions);
+
+  }
+  
+};
