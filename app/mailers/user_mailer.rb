76e22c6f7a9aa0ab0443b228b4a8eb61f73941b1
Franz Josef <franz.josef.bruenner@gmail.com>
2012-09-29 19:48:40 +0200

    just playing with action mailer, cool

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "event-corp@events.com"
   
   def send_contact_mail sub, body
-    mail(:body => body, :to => 'franz.josef.bruenner@aon.at', :subject => sub)
+    @body = body
+    mail(:to => 'franz.josef.bruenner@aon.at', :subject => sub)
   end
   
 end
