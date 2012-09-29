1f32c9010463a22b955481b1edd0e17cda019f30
Franz Josef <franz.josef.bruenner@gmail.com>
2012-09-29 19:35:39 +0200

    sendgrid congiguration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,15 +66,16 @@ EventApp::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   # gmail configurations
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {
-  #  :address              => 'smtp.gmail.com',
-  #  :port                 => 587,
-  #  :domain               => 'gmail.com',
-  #  :user_name            => "",
-  #  :password             => "",
-  #  :authentication       => 'login',
-  #  :enable_starttls_auto => true
-  #}
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'event-corp.herokuapp.com/',
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   
 end
