ece4628cd4a383a6759e00abe87ce135ebef6d9a
Franz Josef <franz.josef.bruenner@gmail.com>
2012-09-29 13:28:23 +0200

    gmail server configuration

--- config/$RAILS_ENV.rb
+++
@@ -1,9 +0,0 @@
-﻿config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'http://event-corp.herokuapp.com/',
-  :user_name            => 'franz.josef.bruenner@gmail.com',
-  :password             => 'ayduof3a',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -1,11 +1,10 @@
-﻿config.action_mailer.delivery_method = :smtp
+﻿#config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "http://event-corp.herokuapp.com/",
-  :user_name            => "franz.josef.bruenner@aon.at",
-  :password             => "ayduof3a",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "google.com",
+  :authentication =>"plain",
+  :user_name => "franz.josef.bruenner@aon.at",
+  :password => "ayduof3a"
 }
 
