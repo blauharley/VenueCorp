3a6ec132c3347723bb764d082bd2f501399c180c
Franz Josef <franz.josef.bruenner@gmail.com>
2012-09-29 18:47:07 +0200

    preparing for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ EventApp::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'http://event-corp.herokuapp.com/',
+  :domain => 'event-corp.herokuapp.com/',
   :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
