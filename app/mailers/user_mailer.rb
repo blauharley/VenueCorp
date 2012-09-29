051d298f8e414ecb0c168d351fc090cd65bab938
Franz Josef <franz.josef.bruenner@gmail.com>
2012-09-29 15:22:56 +0200

    gmail server configuration finished

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "event-corp@events.com"
   
   def send_contact_mail sub, body
-    mail(:body => body, :to => 'franz.josef.bruenner@gmail.com', :subject => sub)
+    mail(:body => body, :to => 'franz.josef.bruenner@aon.at', :subject => sub)
   end
   
 end
--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -1,4 +1,4 @@
-﻿#config.action_mailer.delivery_method = :smtp
+﻿config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
