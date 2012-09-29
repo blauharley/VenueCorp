fde1e3e7925a46d89c7994276590b6db11eb1cd0
Franz Josef <franz.josef.bruenner@gmail.com>
2012-09-29 17:30:05 +0300

    Update config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EventApp::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => "franz.josef.bruenner@gmail.com",
-  :password             => "ayduof3a",
-  :authentication       => 'plain'
-}
