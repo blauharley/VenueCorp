3e0039cda2f0218dcdfefe1f2fe45b3d19c15775
Franz Josef <franz.josef.bruenner@gmail.com>
2012-10-09 13:00:58 +0200

    preparing highlight kinds for event

---
+++ db/migrate/20121009125800_add_highlight_kinds_to_event.rb
@@ -0,0 +1,12 @@
+﻿class AddHighlightKindsToEvent < ActiveRecord::Migration
+  def self.up
+    remove_column :events, :highlight
+    add_column :events, :federal_highlight, :boolean
+    add_column :events, :regional_highlight, :boolean
+  end
+
+  def self.down
+    remove_column :events, :federal_highlight
+    remove_column :events, :regional_highlight
+  end
+end
