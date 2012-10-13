a7da10b14de65067e92723a51051d9e6d445a45d
Franz Josef <franz.josef.bruenner@gmail.com>
2012-10-13 15:30:28 +0200

    migration created: description string to text because of pg on heroku

---
+++ db/migrate/20121013152800_change_string_column_description_to_text_column.rb
@@ -0,0 +1,10 @@
+﻿class ChangeStringColumnDescriptionToTextColumn < ActiveRecord::Migration
+  def self.up
+    remove_column :events, :description
+    add_column :events, :description, :text
+  end
+
+  def self.down
+    remove_column :events, :description
+  end
+end
