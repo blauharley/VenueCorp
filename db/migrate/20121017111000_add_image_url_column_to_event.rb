c19014df98bb155fd2f877aa617bfff559ff80e2
Franz Josef <franz.josef.bruenner@gmail.com>
2012-10-17 11:32:27 +0200

    new migration image_url for event table

---
+++ db/migrate/20121017111000_add_image_url_column_to_event.rb
@@ -0,0 +1,9 @@
+﻿class AddImageUrlColumnToEvent < ActiveRecord::Migration
+  def self.up
+    add_column :events, :image_url, :string
+  end
+
+  def self.down
+    remove_column :events, :image_url
+  end
+end
